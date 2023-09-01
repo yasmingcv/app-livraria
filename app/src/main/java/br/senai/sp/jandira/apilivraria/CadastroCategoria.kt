7a10e4e082ba2463f759b7a2d6c914a535bb7e06
YASMIN GONÇALVES COELHO <22282215@dsn127m1242267.cfp127aluno.local>
2023-09-01 16:43:42 -0300

    comecando

---
+++ app/src/main/java/br/senai/sp/jandira/apilivraria/CadastroCategoria.kt
@@ -0,0 +1,11 @@
+package br.senai.sp.jandira.apilivraria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastroCategoria : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_categoria)
+    }
+}
